b659fafb29afdbd77fdb840d0af3757a9369586f
lianasyrkett <liana.syrkett@yahoo.com>
2017-08-04 21:29:50 -0400

    Final draft of MyTokenReader.java.

--- src/com/google/codeu/mathlang/impl/MyTokenReader.java
+++ src/com/google/codeu/mathlang/impl/MyTokenReader.java
@@ -15,7 +15,13 @@
 package com.google.codeu.mathlang.impl;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
+import com.google.codeu.mathlang.core.tokens.NameToken;
+import com.google.codeu.mathlang.core.tokens.NumberToken;
+import com.google.codeu.mathlang.core.tokens.StringToken;
+import com.google.codeu.mathlang.core.tokens.SymbolToken;
 import com.google.codeu.mathlang.core.tokens.Token;
 import com.google.codeu.mathlang.parsing.TokenReader;
 
@@ -27,99 +33,109 @@ import com.google.codeu.mathlang.parsing.TokenReader;
 // work with the test of the system.
 public final class MyTokenReader implements TokenReader {
 
-  public MyTokenReader(String source) {
-    private static final ArrayList<Character> SYMBOLS = new ArrayList<Character>(Arrays.asList(';', '+', '=', '-'));
+	private static final ArrayList<Character> SYMBOLS = new ArrayList<Character>(
+			Arrays.asList(';', '+', '=', '-'));
 
-private static final char SPACE_CHAR = ' ';
-private static final char NEWLINE_CHAR = '\n';
-private static final char QUOTE_CHAR = '\"';
+	private static final char SPACE_CHAR = ' ';
+	private static final char NEWLINE_CHAR = '\n';
+	private static final char QUOTE_CHAR = '\"';
 
-private String source;
-private int currentIndex = 0;
+	private String source;
+	private int currentIndex = 0;
 
-public MyTokenReader(String source) {
-  // Your token reader will only be given a string for input. The string will
-  // contain the whole source (0 or more lines).
-this.source = source;
-}
+	public MyTokenReader(String source) {
+		this.source = source;
+	}
 
-String GOAL = "note \"comment 1\"\n     \"comment 2\";";
+	@Override
+	public Token next() throws IOException {
+		// This runs when there is no more Tokens in String source.
 
-@Override
-public Token next() throws IOException {
-  // Most of your work will take place here. For every call to |next| you should
-  // return a token until you reach the end. When there are no more tokens, you
-  // should return |null| to signal the end of input.
+		if (source == null) {
+			throw new IOException("Source cannot be null.");
+		}
+		if (source.length() <= currentIndex) {
+			return null;
+		}
 
-  // If for any reason you detect an error in the input, you may throw an IOException
-  // which will stop all execution.
+		Character currentChar = getCurrentChar();
+		if (currentChar == null) {
+			throw new IOException("Invalid source String.");
+		}
 
-if (source.length() <= currentIndex) return null;
+		// Determines if the token is between quotes and returns the
+		// StringToken.
+		if (currentChar == QUOTE_CHAR) {
+			int endQuoteIndex = source.indexOf(QUOTE_CHAR, currentIndex + 1);
+			String str = source.substring(currentIndex + 1, endQuoteIndex)
+					.trim();
 
-Character currentChar = getCurrentChar();
-if (currentChar == null) return null;
+			currentIndex = endQuoteIndex + 1;
 
-if (currentChar == QUOTE_CHAR) {
-  // RETURN A STRING
-  int endQuoteIndex = source.indexOf(QUOTE_CHAR, currentIndex + 1);
-  String str = source.substring(currentIndex + 1, endQuoteIndex).trim();
+			return new StringToken(str);
+		}
 
-  currentIndex = endQuoteIndex + 1;
+		// Finds the index that the SymbolToken would be at.
+		int symbolIndex = -1;
+		int spaceIndex = source.indexOf(SPACE_CHAR, currentIndex + 1);
+		for (Character symbol : SYMBOLS) {
+			int potentialCutoff = source.indexOf(symbol, currentIndex + 1);
+			if (potentialCutoff != -1) {
+				if (symbolIndex == -1) {
+					symbolIndex = potentialCutoff;
+				} else {
+					symbolIndex = Math.min(potentialCutoff, symbolIndex);
+				}
+			}
+		}
 
-  return new StringToken(str);
-}
+		// Determines the index of the cutoff after the NumberToken or
+		// SymbolToken.
+		int cutoffIndex = -1;
+		if (spaceIndex != -1 && symbolIndex != -1) {
+			cutoffIndex = Math.min(symbolIndex, spaceIndex);
+		} else {
+			cutoffIndex = Math.max(symbolIndex, spaceIndex);
+		}
 
-int symbolIndex = -1;
-int spaceIndex = source.indexOf(SPACE_CHAR, currentIndex + 1);
-for (Character symbol : SYMBOLS) {
-    int potentialCutoff = source.indexOf(symbol, currentIndex + 1);
-    if (potentialCutoff != -1) {
-    if (symbolIndex == -1) symbolIndex = potentialCutoff;
-    else symbolIndex = Math.min(potentialCutoff, symbolIndex);
-    }
-  }
+		if (cutoffIndex != -1 && currentChar != '+') {
+			Double number = null;
+			try {
+				number = Double.valueOf(source.substring(currentIndex,
+						cutoffIndex));
+			} catch (NumberFormatException nfe) {
+				number = null;
+			}
 
-int cutoffIndex = -1;
-if (spaceIndex != -1 && symbolIndex != -1) {
-  cutoffIndex = Math.min(symbolIndex, spaceIndex);
-} else {
-  cutoffIndex = Math.max(symbolIndex, spaceIndex);
-}
+			// Determines if it is a NumberToken.
+			if (number != null) {
+				currentIndex = cutoffIndex;
+				return new NumberToken(number);
+			}
+		}
 
-if (cutoffIndex != -1 && currentChar != '+') {
-  Double number = null;
-  try {
-  number = Double.valueOf(source.substring(currentIndex, cutoffIndex));
-  } catch (NumberFormatException nfe) {
-  number = null;
-  }
+		// Determines if it is a SymbolToken.
+		if (SYMBOLS.contains(currentChar)) {
+			currentIndex++;
+			return new SymbolToken(currentChar);
+		} else {
+			// Determines if it is a NameToken.
+			String name = source.substring(currentIndex, cutoffIndex);
 
-  if (number != null) {
-  currentIndex = cutoffIndex;
-  return new NumberToken(number);
-  }
-}
+			currentIndex = cutoffIndex;
+			return new NameToken(name);
+		}
+	}
 
-if (SYMBOLS.contains(currentChar)) {
-  currentIndex++;
-  return new SymbolToken(currentChar);
-} else {
-  String name = source.substring(currentIndex, cutoffIndex);
-
-  currentIndex = cutoffIndex;
-  return new NameToken(name);
-}
-
-}
-
-private Character getCurrentChar() {
-if (currentIndex >= source.length()) return null;
-  char currentChar = source.charAt(currentIndex);
-  if (currentChar == SPACE_CHAR || currentChar == NEWLINE_CHAR) {
-    currentIndex++;
-    return getCurrentChar();
-  }
-  return currentChar;
-}
-}
+  //Helper method to get the character at the current index.
+	private Character getCurrentChar() {
+		if (currentIndex >= source.length())
+			return null;
+		char currentChar = source.charAt(currentIndex);
+		if (currentChar == SPACE_CHAR || currentChar == NEWLINE_CHAR) {
+			currentIndex++;
+			return getCurrentChar();
+		}
+		return currentChar;
+	}
 }
